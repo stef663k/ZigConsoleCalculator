1ac7aa2cc72084c491b45fc9b426ef1d5b4593dc
stef663k <stef663k@elev.techcollege.dk>
2023-03-15 14:29:42 +0100

    Reader/writer

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,8 @@
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var buffer: [20]u8 = undefined;
+
+    var reader = std.io.getStdIn().reader().setBufferSize(buffer[0..].len);
+    var writer = std.io.getStdOut().writer();
+}
