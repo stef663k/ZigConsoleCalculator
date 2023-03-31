4e5abf736cb9de938826266b021e63e216a02a69
stef663k <stef663k@elev.techcollege.dk>
2023-03-31 10:01:26 +0200

    Comments

--- ZigCalculator.zig
+++ ZigCalculator.zig
@@ -1,22 +1,27 @@
 const std = @import("std");
 
 pub fn main() !void {
+    //Get standard input and output
     var input = std.io.getStdIn().reader();
     var output = std.io.getStdOut().writer();
 
+    //Initiating allocator
     var allocator = std.heap.page_allocator;
-    var buffer = try allocator.alloc(u8, 1024);
 
-    defer allocator.free(buffer);
+    //It works without buffer as we allocate the memory in line
+    // var buffer = try allocator.alloc(u8, 1024);
+    // defer allocator.free(buffer);
 
     while (true) {
+
+        //Reads the input from console and allocate memory
         try output.print("Enter you calculation: ", .{});
         var line = try input.readAllAlloc(allocator, 1024);
         if (line.len == 0) break;
-
+        //Parsing the input to float
         var result: f64 = undefined;
         try std.fmt.parseFloat(result, line);
-
+        //Prints the output to console
         try output.print("result: {}\n", .{result});
     }
 }
